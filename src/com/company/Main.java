5a22c0461f5a49a5d3a5376dfff6da0c2c13602f
KALYS'S LAPTOP <bekmenbetov@gmail.com>
2022-02-06 23:35:15 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Class klass = new Class(107,"Agent",new int[]{2,4,5,6});
+
+        System.out.println(klass.getNomer()+" "+ klass.getSoz()+" "+ Arrays.toString(klass.getMassiv()));
+
+
+
+
+    }
+}
